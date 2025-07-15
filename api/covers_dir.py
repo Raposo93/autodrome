a64edad71b9f989e4c57dd5322dae582f0746e07
Gonzalo <gonzalorave7@gmail.com>
2025-07-15 18:17:41 +0200

    Remove unused API endpoints and clean up app.py by unregistering tracks and covers_dir blueprints

--- api/covers_dir.py
+++
@@ -1,9 +0,0 @@
-from flask import send_from_directory, Blueprint
-import os
-
-covers_dir_bp = Blueprint("cover_dir", __name__)
-COVERS_DIR = os.path.abspath("covers")
-
-@covers_dir_bp.route("/<path:filename>")
-def serve_cover(filename):
-    return send_from_directory(COVERS_DIR, filename)
--- app.py
+++ app.py
@@ -1,18 +1,15 @@
 from flask import Flask
 from api.search import search_bp
-from api.tracks import tracks_bp
 from api.download import download_bp
-from api.covers_dir import covers_dir_bp
 from flask_cors import CORS
 
 
 app = Flask(__name__)
+# Configure CORS in production mode
 CORS(app, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True)
 
 app.register_blueprint(search_bp, url_prefix='/api/search')
-app.register_blueprint(tracks_bp, url_prefix='/api/tracks')
 app.register_blueprint(download_bp, url_prefix='/api/download')
-app.register_blueprint(covers_dir_bp, url_prefix='/api/covers_dir')
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
